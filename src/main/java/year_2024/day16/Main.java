53bcfaefd833f17958d4e4ef9688deabc9beddd5
Melnikov Alexander <almelnikov@nota.tech>
2024-12-17 15:02:58 +0300

    2024 day 16 Part 2 optimisation needed

--- src/main/java/year_2024/day16/Main.java
+++ src/main/java/year_2024/day16/Main.java
@@ -1,10 +1,13 @@
 package year_2024.day16;
 
 import common.InputParser;
+import common.Point;
+
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
-    static final int EMPTY = -1;
     static final int WALL = -2;
 
     private static final int ROTATE_COST = 1000;
@@ -16,13 +19,21 @@ public class Main {
                     .map(ch -> ((char) ch) == '#' ? WALL : Integer.MAX_VALUE).toArray())
             .toArray(int[][]::new);
 
+    private static final List<Set<Point>> minPaths = new ArrayList<>();
+
+    private static int minScore;
+
+
     public static void main(String[] args) {
         System.out.println(solve());
+        System.out.println(solve2());
+        printMap();
     }
 
     private static int solve() {
-        goRight(map.length-2, 1, 0);
-        return map[1][map.length-2];
+        goRight(map.length - 2, 1, 0);
+        minScore = map[1][map[1].length - 2];
+        return minScore;
     }
 
     private static void goLeft(int x, int y, int score) {
@@ -76,20 +87,91 @@ public class Main {
         return true;
     }
 
+    private static long solve2() {
+        searchMinPathsRight(map.length - 2, 1, 0, new LinkedHashSet<>());
+        return minPaths.stream()
+                .flatMap(Collection::stream)
+                .distinct().count();
+    }
+
+    private static void searchMinPathsLeft(int x, int y, int currentScore, Set<Point> currentPath) {
+        if (processAndFinish(x, y, currentScore, currentPath)) {
+            return;
+        }
+
+        searchMinPathsUp(x - 1, y, currentScore  + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsDown(x + 1, y, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsLeft(x, y - 1, currentScore + STEP_COST, new LinkedHashSet<>(currentPath));
+    }
+
+    private static void searchMinPathsRight(int x, int y, int currentScore, Set<Point> currentPath) {
+        if (processAndFinish(x, y, currentScore, currentPath)) {
+            return;
+        }
+
+        searchMinPathsUp(x - 1, y, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsDown(x + 1, y, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsRight(x, y + 1, currentScore + STEP_COST, new LinkedHashSet<>(currentPath));
+    }
+
+    private static void searchMinPathsUp(int x, int y, int currentScore, Set<Point> currentPath) {
+        if (processAndFinish(x, y, currentScore, currentPath)) {
+            return;
+        }
+
+        searchMinPathsRight(x, y + 1, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsLeft(x, y - 1, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsUp(x - 1, y, currentScore + STEP_COST, new LinkedHashSet<>(currentPath));
+    }
+
+    private static void searchMinPathsDown(int x, int y, int currentScore, Set<Point> currentPath) {
+        if (processAndFinish(x, y, currentScore, currentPath)) {
+            return;
+        }
+
+        searchMinPathsRight(x, y + 1, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsLeft(x, y - 1, currentScore + STEP_COST + ROTATE_COST, new LinkedHashSet<>(currentPath));
+        searchMinPathsDown(x + 1, y, currentScore + STEP_COST, new LinkedHashSet<>(currentPath));
+    }
+
+    private static boolean processAndFinish(int x, int y, int currentScore, Set<Point> currentPath) {
+        if (currentScore > minScore ||
+            map[x][y] == WALL ||
+            map[x][y] > minScore ||
+            (map[x][y] == minScore && (x != 1 || y != map[1].length - 2)) ||
+            !currentPath.add(new Point(x, y))
+        ) {
+            return true;
+        }
+
+        if (x == 1 && y == map[1].length - 2) {
+            minPaths.add(currentPath);
+            System.out.println(currentScore);
+            System.out.println(currentPath.size());
+            System.out.println();
+            return true;
+        }
+
+        return false;
+    }
+
     private static void printMap() {
+        Set<Point> points = minPaths.stream()
+                .flatMap(Collection::stream)
+                .collect(Collectors.toSet());
+
         for (int x = 0; x < map.length; x++) {
             for (int y = 0; y < map[x].length; y++) {
-                if (map[x][y] == EMPTY) {
-                    System.out.print(" ");
-                } else if (map[x][y] == WALL) {
-                    System.out.print("#");
+                if (map[x][y] == WALL) {
+                    System.out.print("|");
+                } else if (points.contains(new Point(x, y))) {
+                    System.out.print("o");
                 } else {
-                    System.out.print(".");
+                    System.out.print(" ");
                 }
             }
             System.out.println();
         }
         System.out.println();
     }
-
 }
